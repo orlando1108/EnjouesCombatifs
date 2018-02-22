f8bbde2b586b633a290932159ff558e1065d0231
orlando1108 <erwan.raulo2015@campus-eni.fr>
2018-02-22 12:20:20 +0100

    freinage + vitesse 1

--- Assets/erwan_r/Assets/move.cs
+++ Assets/erwan_r/Assets/move.cs
@@ -19,6 +19,9 @@ public class move : MonoBehaviour
     public KeyCode mBack;
     private float acceleration;
     private Vector2 direction;
+    private float accelCoef;
+    private float deccelCoef;
+
 
     // Use this for initialization
     void Start()
@@ -27,9 +30,11 @@ public class move : MonoBehaviour
         speedX = 0.1f;
         speedY = 0.1f;
         speedFrame = 0;
-        speed = 0.02f;
+        speed = 0;
+        accelCoef = 30f;
+        deccelCoef = 50f;
         position = new Vector2(transform.localPosition.x, transform.localPosition.y);
-        acceleration = 0f;
+        acceleration = 0.00001f;
         //transform.Rotate(Vector3.forward, 90);
 
 
@@ -38,63 +43,34 @@ public class move : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
-        Vector3 currentAngle = transform.rotation.eulerAngles;
-        float rotation = currentAngle.z;
-
-
-
-        /*
-        if (Input.GetKey(right))
+        if(speed > 0.005 && !Input.GetKey(mForward))
         {
-            speedFrame++;
-            if (speedFrame == 20)
-            {
-                currentSpriteId = (currentSpriteId + 1) % cavernManList.Length;
-                spRenderer.sprite = cavernManList[currentSpriteId];
-                speedFrame = 0;
-            }
-
-
-        }*/
+            speed -= acceleration * deccelCoef;
+        }
+       
+        transform.position = transform.position + (transform.rotation * Vector3.up) * speed;
+        //avancer 
         if (Input.GetKey(mForward))
         {
-            speed += 0.0001f;
-            //position.x += speedX;
-            /* transform.position = new Vector2(position.x, position.y);
-             transform.position = new Vector2(direction.x, direction.y);*/
-            //transform.position = new Vector2(direction.x += speed, direction.y += speed);
-            transform.position = transform.position + (transform.rotation * Vector3.up * speed);
-
-
+            speed += acceleration * accelCoef;
         }
+        //reculer
         if (Input.GetKey(mBack))
         {
-            speed -= 0.05f;
-            //transform.position = new Vector2(position.x, position.y);*/
+            deccelCoef -= 0.005f;
         }
+        //tourner à droite
         if (Input.GetKey(turnRight))
         {
-            transform.Rotate(Vector3.back * 2);
-           // print(transform.euler);
-            //direction = new Vector2(Mathf.Sin(Mathf.Deg2Rad * transform.rotation) , Mathf.Cos(Mathf.Deg2Rad * ));
+            transform.Rotate(Vector3.back * 2.5f);
             
         }
+        //tourner à gauche
         if (Input.GetKey(turnLeft))
         {
-            transform.Rotate(Vector3.forward * 2);
-            direction = new Vector2(Mathf.Sin(Mathf.Deg2Rad * transform.rotation.x), Mathf.Cos(Mathf.Deg2Rad * transform.rotation.y));
+            transform.Rotate(Vector3.forward * 2.5f);
         }
-       /* if (Input.GetKey(up))
-        {
-            position.y += speedY;
-            transform.position = new Vector2(position.x, position.y);
-        }
-        if (Input.GetKey(down))
-        {
-            position.y -= speedY;
-            transform.position = new Vector2(position.x, position.y);
-        }*/
+       
 
 
     }
