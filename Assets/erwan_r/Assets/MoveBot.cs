4f070f128249e894a20e4e1ed88beaab6d8c97eb
orlando1108 <erwan.raulo2015@campus-eni.fr>
2018-03-09 18:29:41 +0100

    IA sans path

--- Assets/erwan_r/Assets/MoveBot.cs
+++ Assets/erwan_r/Assets/MoveBot.cs
@@ -96,21 +96,27 @@ public class MoveBot : MonoBehaviour
     public ParticleSystem skidEffect;
     public ParticleSystem boostEffect;
     public List<Transform> nodes;
-    float speedForce = 1f;
+    float speedForce = 7f;
     float torqueForce = -200f;
     float driftFactorSticky = 0.5f;
-    float driftFactorSlippy = 0.9f;
+    float driftFactorSlippy = 0.7f;
     float maxStickyVelocity = 2.5f;
     float minStickyVelocity = 1.5f;
     float audioClipSpeed = 6f;
-    public float steering = 4.0f;
+    
     private int currentNode = 0;
+    float targetRot;
     AudioSource motorSound;
     Rigidbody2D bot;
     Vector3 direction;
+   
+
+
+
+    /*public float acceleration = 0.00001f;
+    public float braking = 0.1f;
+    public float steering = 0.001f;*/
     Vector3 target;
-
-
 
     void Start()
     {
@@ -120,27 +126,32 @@ public class MoveBot : MonoBehaviour
         motorSound = GetComponent<AudioSource>();
         motorSound.Play();
         bot = GetComponent<Rigidbody2D>();
+        direction = (nodes[currentNode].position - bot.transform.position);
+        //bot.transform.position = new Vector2(nodes[currentNode].position.x, nodes[currentNode].position.y);
 
     }
     
     void Update()
     {
     }
-    // Update is called once per frame
+
     void FixedUpdate()
     {
         float driftFactor = driftFactorSticky;
         float pitch = bot.velocity.magnitude / audioClipSpeed;
 
+        CheckDistance();
+        SteerTowardsTarget();
+
         skidEffect.emissionRate = 0;
         exhaust.emissionRate = 2;
-        SteerTowardsTarget();
-        CheckDistance();
+        //CheckDistance();
 
         // ACCELERATE
-         direction = (nodes[currentNode].position - bot.transform.position);
-         bot.AddForce(direction.normalized);
-         exhaust.emissionRate = 15;
+        bot.velocity = ForwardVelocity() + RightVelocity() * driftFactorSlippy;
+        direction = (nodes[currentNode].position - bot.transform.position);
+        bot.AddForce(direction.normalized * speedForce);
+        exhaust.emissionRate = 15;
 
         //ROTATE
         //float targetRot = Vector2.Angle(bot.transform.up, direction);
@@ -159,7 +170,7 @@ public class MoveBot : MonoBehaviour
 
         }*/
 
-        if (Input.GetButton("Brakes"))
+       /* if (Input.GetButton("Brakes"))
         {
             bot.AddForce(transform.up * -speedForce / 2);
             skidEffect.emissionRate = 15;
@@ -175,11 +186,18 @@ public class MoveBot : MonoBehaviour
         {
             boostEffect.emissionRate = 0;
             speedForce = 6;
-        }
-        
-        float tf = Mathf.Lerp(0, torqueForce, bot.velocity.magnitude / 5);
-       // bot.angularVelocity = Input.GetAxis("Horizontal") * tf;
-       // bot.AddTorque(Input.GetAxis("Horizontal") * torqueForce);
+        }*/
+
+       
+
+
+        // always accelerate
+        float velocity = bot.velocity.magnitude;
+        //velocity += acceleration;
+
+        // apply car movement
+       /* bot.velocity = transform.up * velocity;
+        bot.angularVelocity = 0.0f;*/
 
 
     }
@@ -192,19 +210,45 @@ public class MoveBot : MonoBehaviour
     {
         return transform.right * Vector2.Dot(GetComponent<Rigidbody2D>().velocity, transform.right);
     }
+    void SteerTowardsTarget()
+    {
+       // Vector2 towardNextTrigger = direction - bot.transform.position;
 
-    private void CheckDistance() {
-       // Debug.Log(" DISTANCE " + Vector3.Distance(bot.transform.position, nodes[currentNode].position));
-        if(Vector3.Distance(bot.transform.position, nodes[currentNode].position) < 0.3f)
+        float targetRot = Vector2.Angle(Vector2.right, direction);
+      /*  Debug.Log("bOT ANGLE  " + bot.transform.eulerAngles);
+        Debug.Log("DIR ANGLE  " + direction);*/
+        Debug.Log("ANGLE  " + targetRot);
+         if (direction.y < 0.0f)
+         {
+             targetRot = -targetRot;
+         }
+        // float rot = Mathf.MoveTowardsAngle(bot.transform.localEulerAngles.z, targetRot, steering);
+        //bot.transform.eulerAngles = new Vector3(0.0f, 0.0f, bot.transform.eulerAngles.z + targetRot);
+
+         bot.transform.localEulerAngles = new Vector3(0, 0, Mathf.LerpAngle(transform.localEulerAngles.z, targetRot -90 , 0.1f));
+       // bot.transform.up = new Vector3(Mathf.Lerp(bot.transform.up.x, direction.x, 0.5f), Mathf.Lerp(bot.transform.up.y, direction.y, 0.5f), Mathf.Lerp(bot.transform.up.z, direction.z, 0.5f));
+       
+    }
+
+    private void CheckDistance()
+    {
+        // Debug.Log(" DISTANCE " + Vector3.Distance(bot.transform.position, nodes[currentNode].position));
+        if (Vector3.Distance(bot.transform.position, nodes[currentNode].position) < 0.5f)
         {
-            if(currentNode == nodes.Count - 1)
+            if (currentNode == nodes.Count - 1)
             {
                 currentNode = 0;
+                /* target = Vector3.Lerp(nodes[currentNode].transform.position - nodes[currentNode].transform.up,
+                               nodes[currentNode].transform.position + nodes[currentNode].transform.up,
+                               Random.value);*/
             }
             else
             {
                 currentNode++;
                 
+                /*target = Vector3.Lerp(nodes[currentNode].transform.position - nodes[currentNode].transform.up,
+                              nodes[currentNode].transform.position + nodes[currentNode].transform.up,
+                              Random.value);*/
 
 
             }
@@ -212,25 +256,11 @@ public class MoveBot : MonoBehaviour
 
     }
 
-    void SteerTowardsTarget()
-    {
-       // Vector2 towardNextTrigger = target - bot.transform.position;
-        float targetRot = Vector2.Angle(bot.transform.up, direction);
-        Debug.Log("ROT  " + targetRot);
 
 
-         /*if (direction.y < 0.0f)
-         {
-             targetRot = -targetRot;
-         }*/
-        //float rot = Mathf.MoveTowardsAngle(transform.localEulerAngles.z, targetRot, 0.5f);
-        // bot.MoveRotation(targetRot);
-        /*float rot = Mathf.MoveTowardsAngle(transform.eulerAngles.z, targetRot, steering);
-        transform.eulerAngles = new Vector3(0.0f, 0.0f, rot);*/
-        //bot.transform.eulerAngles = new Vector3(0, 0, Mathf.LerpAngle(transform.eulerAngles.z, targetRot, 0.5f));
-        transform.eulerAngles = new Vector3(0, 0, Mathf.LerpAngle(transform.eulerAngles.z, targetRot, 0.01f));
 
-    }
+
+
 
 
 }
